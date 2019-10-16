8c1746fd477e73619b8e55ca8e2004f19830c0aa
Dameon Green <dameongreen@gmail.com>
2019-10-16 19:30:30 -0400

    updates

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import CarbonComponentsVue from "@carbon/vue";
+import { createProvider } from './vue-apollo'
 
 
 Vue.use (CarbonComponentsVue);
@@ -9,5 +10,6 @@ Vue.config.productionTip = false;
 
 new Vue({
   router,
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app');
--- src/views/RepoPage/RepoPage.vue
+++ src/views/RepoPage/RepoPage.vue
@@ -1,3 +1,35 @@
 <template>
-    <div>REPO PAGE</div>
+  <div class="bx--grid bx--grid--full-width bx--grid--no-gutter repo-page">
+    <div class="bx--row repo-page__r1">
+      <div class="bx--col-lg-16">
+        <repo-table
+        :headers="headers"
+        :rows="rows"
+        title="Carbon Repositories"
+        helperText="A collection of public Carbon repositories."
+        />
+        </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'RepoPage',
+  components: { RepoTable },
+  data() {
+    return {
+      headers,
+      rows,
+    };
+  },  
+}
+</script>
+
+<style lang="scss">
+@import '../../styles/carbon-utils';
+.repo-page .bx--row {
+  padding-top: $spacing-05;
+  padding-bottom: $spacing-05;
+}
+</style>
